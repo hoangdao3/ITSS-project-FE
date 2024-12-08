0ff4974dc33f3c7bcf9504b58d3e1fc6a6d90c69
lqviettt <quocviet30042003@gmail.com>
2024-12-08 23:20:14 +0700

    add view login, register, changepass

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,51 +5,63 @@ import { MdSupportAgent } from "react-icons/md";
 import { FaListUl } from "react-icons/fa";
 import { IoIosLogOut } from "react-icons/io";
 import "./Calendar.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Sidebar = () => (
-  <div className="bg-gray-100 w-74 h-screen p-4 flex flex-col justify-between">
-    <div>
-      <div className="flex items-center mb-8">
-        <div className="w-12 h-12">
-          <img
-            src="/logo.png"
-            alt="Logo"
-            className="w-full h-full object-contain"
-          />
+const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="bg-gray-100 w-74 h-screen p-4 flex flex-col justify-between">
+      <div>
+        <div className="flex items-center mb-8">
+          <div className="w-12 h-12">
+            <img
+              src="/logo.png"
+              alt="Logo"
+              className="w-full h-full object-contain"
+            />
+          </div>
+          <span className="ml-4 text-3xl font-bold">運動カレンダー</span>
         </div>
-        <span className="ml-4 text-3xl font-bold">運動カレンダー</span>
+        <nav>
+          <ul>
+            <li className="mb-4 flex items-center cursor-pointer">
+              <i className="fas fa-calendar-alt text-xl"></i>
+              <FaCalendarAlt />
+              <span className="ml-2 text-xl flex">Calendar</span>
+            </li>
+            <li className="mb-4 flex items-center cursor-pointer">
+              <i className="fas fa-dumbbell text-xl"></i>
+              <FaListUl />
+              <span className="ml-2 text-xl">List Of Exercises</span>
+            </li>
+            <li className="mb-4 flex items-center cursor-pointer">
+              <i className="fas fa-comments text-xl"></i>
+              <MdSupportAgent />
+              <span className="ml-2 text-xl">Consults</span>
+            </li>
+            <li className="mb-4 flex items-center cursor-pointer">
+              <i className="fas fa-cog text-xl"></i>
+              <IoSettingsOutline />
+              <span className="ml-2 text-xl">Settings</span>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li className="mb-4 flex items-center cursor-pointer">
-            <i className="fas fa-calendar-alt text-xl"></i>
-            <FaCalendarAlt />
-            <span className="ml-2 text-xl flex">Calendar</span>
-          </li>
-          <li className="mb-4 flex items-center cursor-pointer">
-            <i className="fas fa-dumbbell text-xl"></i>
-            <FaListUl />
-            <span className="ml-2 text-xl">List Of Exercises</span>
-          </li>
-          <li className="mb-4 flex items-center cursor-pointer">
-            <i className="fas fa-comments text-xl"></i>
-            <MdSupportAgent />
-            <span className="ml-2 text-xl">Consults</span>
-          </li>
-          <li className="mb-4 flex items-center cursor-pointer">
-            <i className="fas fa-cog text-xl"></i>
-            <IoSettingsOutline />
-            <span className="ml-2 text-xl">Settings</span>
-          </li>
-        </ul>
-      </nav>
+      <button
+        type="button"
+        onClick={handleLogout}
+        className="bg-gray-500 text-white py-2 px-4 rounded flex items-center"
+      >
+        <IoIosLogOut />
+        <span className="ml-2 text-xl">Logout</span>
+      </button>
     </div>
-    <button className="bg-gray-500 text-white py-2 px-4 rounded flex items-center">
-      <i className="fas fa-sign-out-alt"></i>
-      <IoIosLogOut />
-      <span className="ml-2 text-xl">Logout</span>
-    </button>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
